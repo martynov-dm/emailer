c614236957d1587172ab94aecfedff6bf07cd63f
Dmitrii Martynov <dmartynov@griddynamics>
2022-10-06 15:47:49 +0200

    commit

--- api/kafka/job-status-consuner.js
+++ api/kafka/job-status-consuner.js
@@ -1,5 +1,5 @@
 import Kafka from "node-rdkafka";
-import {EMAIL_PROCESS_TOPIC, EMAIL_TRIGGER_TOPIC} from "./topic-names.js";
+import { EMAIL_PROCESS_TOPIC } from "./topic-names.js";
 import { updateState } from "../jobs-state/jobs-state.js";
 import url from "../urls.js";
 
@@ -31,37 +31,16 @@ consumer.on('ready', (info) => {
     consumer.subscribe([EMAIL_PROCESS_TOPIC]);
     consumer.consume();
 }).on('data', function(data) {
-    // const stateStr = data.value.toString()
-    // const state  = JSON.parse(stateStr)
+    const stateStr = data.value.toString()
+    const state  = JSON.parse(stateStr)
 
-    console.log('received job state ' + data)
+    console.log('received job state ' + stateStr)
 
-    // updateState(state)
+    updateState(state)
 });
 
 export const connectConsumer = () => {
     if (consumer.isConnected()) return
-
-    const stream = Kafka.Producer.createWriteStream({
-        'metadata.broker.list': url.kafka
-    }, {}, {
-        topic: EMAIL_PROCESS_TOPIC
-    });
-
-    stream.on('error', (err) => {
-        console.error('Error in our kafka stream');
-        console.error(err);
-    });
-
-    setInterval(() => {
-        const success = stream.write(Buffer.from('test'));
-        if (success) {
-            console.log(`message queued (test)`);
-        } else {
-            console.log('Could not send message');
-        }
-    }, 1000)
-
 
     consumer.connect();
 }
